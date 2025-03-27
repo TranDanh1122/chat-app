0ce8dd50ad71205fe9880d266f02524e76a1ba0a
TranDanh1122 <trandanh14042000@gmail.com>
2025-03-27 11:00:13 +0700

    update router file

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,54 +1,53 @@
 import { createBrowserRouter } from "react-router-dom";
-import LayoutAuth from "@/layout/Auth/Layout";
 import RouterHandle from "./RouterHandle";
-import { NotFound } from "@/pages/NotFound";
-import AppLayout from "@/layout/App/Layout";
 import React from "react";
 import GlobalLoading from "@/components/app/Shared/GlobalLoading";
+const LayoutAuth = React.lazy(() => import("@/layout/Auth/Layout"));
+const AppLayout = React.lazy(() => import("@/layout/App/Layout"))
 const LoginView = React.lazy(() => import("@/pages/Auth/Login/view/Login"));
 const NewPassView = React.lazy(() => import("@/pages/Auth/NewPass/view/NewPass"));
 const RegisterView = React.lazy(() => import("@/pages/Auth/Register/view/Register"));
 const ResetPassView = React.lazy(() => import("@/pages/Auth/ResetPass/view/ResetPass"));
 const HomeView = React.lazy(() => import("@/pages/App/Home/view/Home"));
 const CreatePostView = React.lazy(() => import("@/pages/App/Post/view/CreatePost"));
-
+const NotFound = React.lazy(() => import("@/pages/NotFound/view/NotFound"));
 export const Router = createBrowserRouter([
     {
         element: <RouterHandle />,
         path: "/",
         children: [
             {
-                element: <LayoutAuth />,
+                element: <React.Suspense fallback={<GlobalLoading />}><LayoutAuth /></React.Suspense>,
                 path: "auth",
                 children: [
                     {
-                        element: <React.Suspense fallback={<GlobalLoading/>}><LoginView /></React.Suspense>,
+                        element: <LoginView />,
                         path: "login"
                     },
                     {
-                        element: <React.Suspense fallback={<GlobalLoading />}><RegisterView /></React.Suspense>,
+                        element: <RegisterView />,
                         path: "register"
                     },
                     {
-                        element: <React.Suspense fallback={<GlobalLoading />}><ResetPassView /></React.Suspense>,
+                        element: <ResetPassView />,
                         path: "reset-password"
                     },
                     {
-                        element: <React.Suspense fallback={<GlobalLoading />}><NewPassView /></React.Suspense>,
+                        element: <NewPassView />,
                         path: "change-password"
                     }
                 ]
             },
             {
-                element: <AppLayout />,
+                element: <React.Suspense fallback={<GlobalLoading />}> <AppLayout /> </React.Suspense>,
                 path: "",
                 children: [
                     {
-                        element: <React.Suspense fallback={<GlobalLoading />}><HomeView /></React.Suspense>,
+                        element: <HomeView />,
                         index: true,
                     },
                     {
-                        element: <React.Suspense fallback={<GlobalLoading />}><CreatePostView /></React.Suspense>,
+                        element: <CreatePostView />,
                         path: "create-post"
                     },
                     {
@@ -58,7 +57,7 @@ export const Router = createBrowserRouter([
                 ]
             },
             {
-                element: <NotFound />,
+                element: <React.Suspense fallback={<GlobalLoading />}><NotFound /></React.Suspense>,
                 path: "*"
             }
         ]
